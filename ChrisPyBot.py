4f954425ec167e3376d0f5657252bf046ee1439e
Chris P <christian.pickler@gmail.com>
2016-12-07 18:38:57 -0800

    Added requirements file with dependencies, guildwars cog with command to get server.

--- ChrisPyBot.py
+++ ChrisPyBot.py
@@ -3,8 +3,25 @@ import discord
 
 import json
 import sys
+import asyncio
+import gw2api.v2
 
 description="""ChrisPy-Bot"""
+
+
+extensions = [
+    'cogs.guildwars'
+]
+
+bot = commands.Bot(command_prefix='!', description=description)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as:')
+    print('Username: ' + bot.user.name)
+    print('ID: ' + bot.user.id)
+    print('------')
 
 
 def load_credentials():
@@ -12,4 +29,10 @@ def load_credentials():
         return json.load(f)
 
 if __name__ == '__main__':
-    bot = commands.Bot(command_prefix='!', description=description)
+    credentials = load_credentials()
+    token = credentials['token']
+
+    for extension in extensions:
+        bot.load_extension(extension)
+
+    bot.run(token)
---
+++ cogs/guildwars.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+import discord
+from guildwars2api.v2 import GuildWars2API as GW2
+from guildwars2api.v1 import GuildWars2API as GW1
+
+api = GW1()
+
+
+def world_name(wid):
+    for i in api.world_names.get():
+        if i['id'] == str(wid):
+            return i['name']
+    return "Error"
+
+
+class GuildWars:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def world(self, ctx, tkn):
+        """
+        Returns the world of the provided API key.
+        :param tkn: Your API key in quotes.
+        """
+        member = ctx.message.author
+        user = GW2(api_key=tkn)
+        wid = user.account.get()["world"]
+        wname = world_name(wid)
+        await self.bot.say('{member.mention} is on world: {world}.'.format(member=member, world=wname))
+
+
+def setup(bot):
+    bot.add_cog(GuildWars(bot))
