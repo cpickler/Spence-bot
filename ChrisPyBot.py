fc8f29c48f0c04b48e0d743c1325ff87f580d069
Chris P <christian.pickler@gmail.com>
2016-12-08 20:24:05 -0800

    Added command to add/update server in database.

--- ChrisPyBot.py
+++ ChrisPyBot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import discord
 
 import json
-import sqlalchemy
+import BotBase as Db
 
 description="""ChrisPy-Bot"""
 
@@ -27,11 +27,14 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def addserver(ctx):
     server = ctx.message.server
-    roles = ctx.message.server.roles
-    print("{} : {}".format(server.name, server.id))
-    for role in roles:
-        print("{} : {}".format(role.name, role.id))
-    await bot.say("Done.")
+    result = Db.add_server(server.id, server.name)
+    if result:
+        msg = 'Server successfully added.'
+    elif result is False:
+        msg = 'Server succesfully updated.'
+    elif result is None:
+        msg = 'No changes made, server may already exist.'
+    await bot.say(msg)
 
 def load_credentials():
     with open('credentials.json') as f:
