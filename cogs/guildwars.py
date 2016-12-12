0aab526b0eb8666ad5ecafe296bf7b2aacae4f7d
cpickler <cpickler@users.noreply.github.com>
2016-12-12 15:42:34 -0800

    Spence bot 3 (#4)
    * Cleaned code with inspection
    
    * Now use discord.utils.get to get role instead of custom function
    
    * Automatic World Role Adding (#2)
    
    * Added function to search for the World Role
    
    * completed and tested function to add World Role
    
    * [Completes #135901501] Have API keys only be added in PMs
    
    * WorldRoles can now only be added by administrators and can not be done in PMs.
    
    * Added function to convert chat codes to item ids
    
    * [completed #135907855] API keys are now validated against token_info api
    
    * Refactored duplicate code in BotBase
    
    * Spence-Bot-3 Guild Role Linking
    Added functions to link Guilds in Database
    Added function to printing out guild memberships
    
    * #3 Added a help docstring to !guilds
    
    * #3 Added a memoization for guild info.
    
    * #3 Completed, guild roles are added if they exist.

--- cogs/guildwars.py
+++ cogs/guildwars.py
@@ -1,25 +1,35 @@
-from discord.ext import commands
+import base64
+import codecs
+
 import discord
-from guildwars2api.v2 import GuildWars2API as GW2
+import guildwars2api
+from discord.ext import commands
 from guildwars2api.v1 import GuildWars2API as GW1
+from guildwars2api.v2 import GuildWars2API as GW2
+
 import BotBase as Db
 
-api = GW1()
+api1 = GW1()
+api2 = GW2()
 
 
 def world_name(wid):
-    for i in api.world_names.get():
+    for i in api1.world_names.get():
         if i['id'] == str(wid):
             return i['name']
     return "Error"
 
-def get_role(server, rname):
-    roles = server.roles
-    for role in roles:
-        if role.name == rname:
-            return role
+
+def chat_to_id(chat):
+    b64 = base64.b64decode(chat)[2:][::-1]
+    hex_string = '0x' + str(codecs.encode(b64, 'hex'))[-5:-1]
+    return int(hex_string, 0)
 
 
+# def is_valid_key(key):
+
+
+# noinspection PyPep8Naming,PyPep8Naming,PyPep8Naming
 class GuildWars:
     def __init__(self, bot):
         self.bot = bot
@@ -30,13 +40,20 @@ class GuildWars:
         Returns the world of the saved API key.
         """
         member = ctx.message.author
+        server = ctx.message.server
         tkn = Db.get_key(member.id)
         if tkn is None:
             await self.bot.say("Error! No api token saved for user {}.".format(member.mention))
         else:
             user = GW2(api_key=tkn)
+            print(user.token_info)
             wid = user.account.get()["world"]
             wname = Db.get_world(wid)
+            # Check to see if the world has a role on the server
+            rid = Db.get_world_role(server.id, wid)
+            if rid is not None:
+                role = discord.utils.get(server.roles, id=str(rid))
+                await self.bot.add_roles(member, role)
             await self.bot.say('{member.mention} is on world: **{world}**.'.format(member=member, world=wname))
 
     @commands.command(pass_context=True)
@@ -47,9 +64,20 @@ class GuildWars:
         :param tkn: Your API token in "quotes."
         :return: "Success!" or "Failed :("
         """
-        uid = ctx.message.author.id
-        msg = Db.add_key(int(uid), tkn)
-        await self.bot.say(msg)
+        # Check to see if the message is a PM, delete if not
+        if ctx.message.server is not None:
+            await self.bot.delete_message(ctx.message)
+            await self.bot.send_message(ctx.message.author, 'API Keys can only be added in PMs.  '
+                                                            'Reply with "!addKey <your api key>" to add your key.')
+        else:
+            try:
+                user = GW2(api_key=tkn)
+                user.token_info.get()
+                uid = ctx.message.author.id
+                msg = Db.add_key(int(uid), tkn)
+            except guildwars2api.base.GuildWars2APIError:
+                msg = "Invalid API key, please try again."
+            await self.bot.say(msg)
 
     @commands.command(pass_context=True)
     async def delKey(self, ctx):
@@ -61,16 +89,66 @@ class GuildWars:
         else:
             await self.bot.say("The api key for {} could not be deleted since it doesn't exist.")
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, no_pm=True)
+    @commands.has_permissions(administrator=True)
     async def addWorldRole(self, ctx, rname):
+
         sid = ctx.message.server.id
         wid = Db.get_world_id(rname)
-        role = get_role(ctx.message.server, rname)
-        Db.add_world_role(sid, role.id, wid)
-        await self.bot.say("Role **{}** successfully linked to world.".format(rname))
+        role = discord.utils.get(ctx.message.server.roles, name=rname)
+        if role is None:
+            await self.bot.say('No role named "{}", verify the role exists, or check your spelling.'.format(rname))
+        else:
+            Db.add_world_role(sid, role.id, wid)
+            await self.bot.say("Role **{}** successfully linked to world.".format(rname))
 
+    @commands.command(pass_context=True, no_pm=True)
+    @commands.has_permissions(administrator=True)
+    async def addGuildRole(self, ctx, rname=None, gnum=None):
+        """
+        Link a guild and a role together. You must have permissions to use this command.
+        :param role: @mention or name of the role to add.
+        :return: Series of prompts to add the role.
+        """
+        author = ctx.message.author
+        user = GW2(api_key=Db.get_key(author.id))
+        guilds = user.account.get()['guilds']
 
+        if rname is None and gnum is None:
+            prompt = 'Which guild to add? \n```'
+            for i in range(len(guilds)):
+                g = api1.guild_details.get(guild_id=guilds[i])
+                prompt += '\n ({num}) {tag:<6} {name}'.format(num=i, tag='['+g['tag']+']', name=g['guild_name'])
 
+            prompt += '\n\n Reply with eg. !addGuildRole "Awesome Guild" 1```'
+            await self.bot.say(prompt)
+
+        elif rname is not None and gnum is not None:
+            role = discord.utils.get(ctx.message.server.roles, name=rname)
+            gid = guilds[int(gnum)]
+            guild = api1.guild_details.get(guild_id=gid)
+            Db.add_guild_role(gid, int(role.id), int(ctx.message.server.id))
+            await self.bot.say("Role successfully added.")
+
+    @commands.command(pass_context=True)
+    async def guilds(self, ctx):
+        """
+        Returns a list of guild memberships for the user.
+        """
+        author = ctx.message.author
+        user = GW2(api_key=Db.get_key(author.id))
+        guilds = user.account.get()['guilds']
+        result = '{} is in guilds:\n```'.format(author.mention)
+        for i in range(len(guilds)):
+            g = Db.get_add_guild(guilds[i])
+            result += '\n ({num}) {tag:<6} {name}'.format(num=i, tag='['+g['tag']+']', name=g['guild_name'])
+            # Add guild role to the user
+            rid = Db.get_guild_role(ctx.message.server.id, guilds[i])
+            if rid is not None:
+                role = discord.utils.get(ctx.message.server.roles, id=str(rid))
+                await self.bot.add_roles(author, role)
+        result += '\n```'
+        await self.bot.say(result)
 
 def setup(bot):
     bot.add_cog(GuildWars(bot))
